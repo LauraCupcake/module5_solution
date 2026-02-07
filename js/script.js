62e98086b85fbbe616ee94e865182305243dc7af
Yu Wah Phyo <yuwahphyo@gmail.com>
2026-02-07 22:49:51 +0630

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,15 +9,15 @@ $(function () {
 (function (global) {
   var dc = {};
 
-  var homeHtmlUrl = "snippets/home-snippet.html";
+  var homeHtmlUrl = "./snippets/home-snippet.html";
   var allCategoriesUrl =
     "https://davids-restaurant.herokuapp.com/categories.json";
-  var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-  var categoryHtml = "snippets/category-snippet.html";
+  var categoriesTitleHtml = "./snippets/categories-title-snippet.html";
+  var categoryHtml = "./snippets/category-snippet.html";
   var menuItemsUrl =
     "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-  var menuItemsTitleHtml = "snippets/menu-items-title.html";
-  var menuItemHtml = "snippets/menu-item.html";
+  var menuItemsTitleHtml = "./snippets/menu-items-title.html";
+  var menuItemHtml = "./snippets/menu-item.html";
 
   // Insert HTML
   function insertHtml(selector, html) {
